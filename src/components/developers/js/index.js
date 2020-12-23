9b6b69d93df1b5fc9dd131f63ec03a30dd662dce
SergeyVishnevskiy <70287472+SergeyVishnevskiy@users.noreply.github.com>
2020-12-23 17:30:27 +0200

    123

--- src/components/developers/js/index.js
+++ src/components/developers/js/index.js
@@ -1,7 +1,5 @@
 import '../css/styles.css';
-// import devTemplates from '../templates/dev-templates.hbs';
 import { modalBackDrop } from '../modal-window/dev-modal';
-// import { modalBackDrop } from '../../modal/modalBackDrop';
 
 const studentBtn = document.querySelector('.js-students');
 studentBtn.addEventListener('click', hendelOpenStudentModal);
